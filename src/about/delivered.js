32473b0dad1cc06911d1dc5b170932aa5f2ffd33
Khanh Nguyen <vkhanh2012@gmail.com>
2026-08-02 23:55:30 +0700

    Chỉnh sửa delivered

--- src/about/delivered.js
+++ src/about/delivered.js
@@ -1,6 +1,6 @@
 import aboutData from "../data/about.json"
 import { renderDeliveredFeatureComponent } from "../components/deliveredfeature.js"
-
+import { nextArrow } from "../components/icons.js"
 export function renderDeliveredFeature() {
   const deliveredFeatureHtml = aboutData.deliveryFeatures
     .map((item) => renderDeliveredFeatureComponent(item))
@@ -25,13 +25,9 @@ export function renderDeliveredFeature() {
             </div>
             <!-- Button Shop Now -->
              <div class="inline-flex items-center px-8 py-3.5 md:px-10 md:py-4 rounded-pill bg-primary text-white font-semibold text-sm gap-4 hover:bg-primary-dark cursor-pointer">
-                Shop Now
-                  <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15.75 6.77539H0.75" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M9.7002 0.75L15.7502 6.774L9.7002 12.799" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                  </svg>
-                <a href="./shop.html"></a>
+               ${aboutData.delivered.buttonText}
+                ${nextArrow}
+                <a href="${aboutData.delivered.buttonLink}"></a>
               </div>
             </div>
           <!-- Cột 2: hình ảnh -->
